43105313c690f466bde40cb7128dfc3d06185d8b
EvelynMS1 <102782127+EvelynMS1@users.noreply.github.com>
2023-05-10 18:47:37 -0700

    created server file in repo

---
+++ server.js
@@ -0,0 +1,4 @@
+// create basic route for the fetch to api to search for book that user requests 
+//sends back whether book id available if yes then displays the book cover 
+
+//process of buying initiates option in the cover display add to checkout 
