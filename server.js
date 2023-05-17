93f3c3f8d60673c6bca3d7b118c00f5af215eede
hicham <hicha.taha@gmail.com>
2023-05-16 20:16:29 -0700

    init repo

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
-// create basic route for the fetch to api to search for book that user requests 
-//sends back whether book id available if yes then displays the book cover 
+var express = require("express");
+var path = require("path")
 
-//process of buying initiates option in the cover display add to checkout 
+var session = require("express-session");
+
+var passport = require("./config/passport");
+
+// Compress
+var compression = require('compression')
+
+
+// Sets up the Express App
+
+var app = express();
+var PORT = process.env.PORT || 3001;
+
+
+app.use(compression())
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
