776bfddb144a5be1bd3271136ac827cf3fb8261f
EvelynMS1 <102782127+EvelynMS1@users.noreply.github.com>
2023-05-23 14:42:27 -0700

    Changes for the homepage cards

--- public/js/bookdisplay.js
+++ public/js/bookdisplay.js
@@ -1,137 +1,142 @@
-  const frontCard = document.querySelectorAll(".flip-card-front");
-  const bookdiv = document.querySelector(".booklayout");
-  const btncheckout = document.querySelectorAll(".checkoutBtnn");
+const frontCard = document.querySelectorAll(".flip-card-front");
+const bookdiv = document.querySelector(".booklayout");
+const btncheckout = document.querySelectorAll(".checkoutBtnn");
 const authorN = document.querySelector(".authorName");
 const mainCard = document.querySelector(".flip-card-inner");
 const cardholder = document.querySelector(".flip-card");
 
 //calling fetch function  
-  const bookFormHandler = async(event)=>{ 
-  const booktitlesearch = document.querySelector("#title").value.trim(); 
+const bookFormHandler = async (event) => {
+  const booktitlesearch = document.querySelector("#title").value.trim();
   console.log('bookFormHandler');
   //getting value from search bar
- console.log(booktitlesearch);
+  console.log(booktitlesearch);
   logJSONData(booktitlesearch);
 };
-  async function logJSONData(title) {
-    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${title}&maxResults=5&key=AIzaSyD0kfX1gVi9NATkHXSkfYYuf2pwOes1C_U`);
-    const booklist = await response.json();
-    //displaying the results of fetch call 
- 
- const bookitemarray = await addValuestoArray(booklist);
- console.log(bookitemarray);
- addValuestoCard(bookitemarray);
-   
-  };  
- async function addValuestoArray(booklist){
-  const booksdisplayed = [booklist.items[0],booklist.items[1],booklist.items[2],booklist.items[3],booklist.items[4]];
+async function logJSONData(title) {
+  const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${title}&maxResults=5&key=AIzaSyD0kfX1gVi9NATkHXSkfYYuf2pwOes1C_U`);
+  const booklist = await response.json();
+  //displaying the results of fetch call 
+
+  const bookitemarray = await addValuestoArray(booklist);
+  console.log(bookitemarray);
+  addValuestoCard(bookitemarray);
+
+};
+async function addValuestoArray(booklist) {
+  const booksdisplayed = [booklist.items[0], booklist.items[1], booklist.items[2], booklist.items[3], booklist.items[4]];
   console.log(booksdisplayed);
   const resultarray = [];
   //fucntion that goes through each item in the array and then calls the function within it
-  for (var i = 0; i < booksdisplayed.length; i++){ 
+  for (var i = 0; i < booksdisplayed.length; i++) {
     const fetchdata = booksdisplayed[i];
     console.log(fetchdata);
     bookinfo = fetchdata.volumeInfo;
-    title = bookinfo.title;    
-    authors= bookinfo.authors;
+    title = bookinfo.title;
+    authors = bookinfo.authors;
     thumbnailImg = bookinfo.imageLinks.thumbnail;
     thumbnailSmlImg = bookinfo.imageLinks.smallThumbnail;
     rating = bookinfo.averageRating;
     categories = bookinfo.categories[0];
-    if(categories){
+    if (categories) {
       console.log(categories);
     } else {
       console.log('');
     }
-    if (rating){
+    if (rating) {
       console.log(rating);
-    }else {
+    } else {
       console.log('No ratings');
-    } 
+    }
     const obj = {
-      booktitle:title,
-      bookauthor:authors,
-      img:thumbnailImg,
+      booktitle: title,
+      bookauthor: authors,
+      img: thumbnailImg,
       bookrating: rating,
-      bookcategories:categories
+      bookcategories: categories
     }
 
     // const eachbook = bookProperties(booksdisplayed[i]);
     resultarray.push(obj);
   }
   return resultarray;
-    //create a function that gets all values from api 
-    //have an array for each index of the call that is called the function for each 
-    //create an object for all of the values that are for this specific book access the values by the properties and dot syntax
-  };
 
-//get values from the array that is returned and place them accordingly
-//call another function that will add the values to appropriate place on html 
-//takes array of object
- async function addValuestoCard(bookArray){
- 
- 
+};
+
+
+async function addValuestoCard(bookArray) {
+
+
   console.log(bookArray);
-  // const author = bookArrayj[0].bookauthor;
-  // const bTitle = bookArray[0].booktitle;
-  // const bcategories = bookArray[0].bookcategories;
-  // const brating = bookArray[0].bookrating;
-  // const bimg = bookArray[0].img;
-  // console.log(authorfromobj);
 
-// authorN.textContent = author;
 
-//have an array that has all of the object for each book
- bookArray.forEach((item,index)=>{
-//main card holder of two divs with class of flip card
-  const card = frontCard[index];
-  card.classList.add('btnfront');
- //items created for the front of card
- const button = document.createElement('button');
- button.classList.add('checkoutBtn');
- button.textContent = 'Add to Cart';
- 
- const wishbutton = document.createElement('button');
- wishbutton.classList.add('wishlistBtn');
- wishbutton.textContent = 'Add to Wishlist';
-  const author = document.createElement('p');
+  //have an array that has all of the object for each book
+  bookArray.forEach((item, index) => {
+    //main card holder of two divs with class of flip card
+    const card = frontCard[index];
+    card.classList.add('btnfront');
+    //creating buttons checkout and wishlist for front card
+    const button = document.createElement('button');
+    button.classList.add('checkoutBtn');
+    button.textContent = 'Add to Cart';
+    const wishbutton = document.createElement('button');
+    wishbutton.classList.add('wishlistBtn');
+    wishbutton.textContent = 'Add to Wishlist';
+    //create elements p and img for storing content
+    const author = document.createElement('p');
+    author.classList.add('author');
+    author.textContent = item.bookauthor;
+    const title = document.createElement('p');
+    title.classList.add('title');
+    title.textContent = item.booktitle;
+    const img = document.createElement('img');
+    img.setAttribute('src', item.img);
 
-  author.textContent = item.bookauthor;
-  const title = document.createElement('p');
-  title.classList.add('.value');
-  title.textContent = item.booktitle;
-  const img = document.createElement('img');
-  img.setAttribute('src', item.img);
-  
-  //element for the back of the card  
-  // const mainCard = document.createElement('div');
+   
 
-  //for the 
- 
-  card.appendChild(title);  
-  card.appendChild(img);
-  card.appendChild(author);
-  card.appendChild(button); 
-  card.appendChild(wishbutton);
+    card.appendChild(title);
+    card.appendChild(img);
+    card.appendChild(author);
+    card.appendChild(button);
+    card.appendChild(wishbutton);
 
-  
-  card.addEventListener('click', function(event){
-    const target = event.target;
-    if(target.classList.contains('checkoutBtn')){
-      const div = target.closest('.btnfront');
-      const savedtitle = div.querySelector('p');
-      const storedtitle = savedtitle.textContent;
-      document.location.replace('/checkout');
-      //save to local storage
-      sessionStorage.setItem('userbooktitle',storedtitle);
-      // sessionStorage.setItem()
-      console.log(storedtitle);
 
-    }
-  })
+    card.addEventListener('click', function (event) {
+      const target = event.target;
+      if (target.classList.contains('checkoutBtn')) {
+        const div = target.closest('.btnfront');
+        const savedtitle = div.querySelector('.checkoutBtn');
+        const storedtitle = savedtitle.textContent;
+        document.location.replace('/checkout');
+        //save to local storage
+        sessionStorage.setItem('userbooktitle', storedtitle);
+        // sessionStorage.setItem()
+        console.log(storedtitle);
+
+      }
+    })
+
+
+    card.addEventListener('click', function (event) {
+      const target = event.target;
+      if (target.classList.contains('wishlistBtn')) {
+        const div = target.closest('.btnfront');
+        const savedtitle = div.querySelector('p');
+        const storedtitle = savedtitle.textContent;
+
+        document.location.replace('/wishlist');
+
+        //save to local storage
+        sessionStorage.setItem('userbooktitle', storedtitle);
+        // sessionStorage.setItem()
+        console.log(storedtitle);
+
+      }
+    })
+
 
   });
- };
+};
 
 
 
@@ -140,18 +145,18 @@ document
   .addEventListener('click', bookFormHandler)
 document
   .querySelector('#checkoutBtn1')
-  .addEventListener('click',bookFormHandler)
+  .addEventListener('click', bookFormHandler)
 document
   .querySelector('#checkoutBtn2')
-  .addEventListener('click',bookFormHandler)
+  .addEventListener('click', bookFormHandler)
 document
   .querySelector('#checkoutBtn3')
-  .addEventListener('click',bookFormHandler)
+  .addEventListener('click', bookFormHandler)
 document
   .querySelector('#checkoutBtn4')
-  .addEventListener('click',bookFormHandler)
+  .addEventListener('click', bookFormHandler)
 document
   .querySelector('#checkoutBtn5')
-  .addEventListener('click',bookFormHandler)
- 
+  .addEventListener('click', bookFormHandler)
+
 
